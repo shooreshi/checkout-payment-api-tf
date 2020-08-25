961f8f81df673f8686048b07be6076b13975c029
Shooresh Golzari <s.golzari@gmail.com>
2020-08-25 18:54:32 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "sub" { type = string }
+variable "client_secret" { type = string }
+variable "client_id" { type = string }
+variable "tenant_id" { type = string }
+variable "tf_version" { type = string }
