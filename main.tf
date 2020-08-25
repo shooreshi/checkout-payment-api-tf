f0de5f344144a61c310ff31b4abcd9d232b4cad0
Shooresh Golzari <s.golzari@gmail.com>
2020-08-25 19:32:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "payment-api" {
     name = "payment-api-rg"
-    location = "uksouth"
+    location = "westeurope"
 }
 
 resource "azurerm_kubernetes_cluster" "payment-api" {
